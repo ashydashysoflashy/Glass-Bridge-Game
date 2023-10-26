20ef9d51b0ac18f7c5179ed48ea9639bf2711e91
Achraf <achraf.abdelrhafour@ucalgary.ca>
2023-10-25 18:32:43 -0600

    Initial Commit: All files from part 1
    JavaScript Function Headers Only: No functionality implemented yet

---
+++ scripts.js
@@ -0,0 +1,51 @@
+/* Course: SENG 513 */
+/* Date: OCT 23, 2023 */
+/* Assignment 2 Part One */
+/* Name: Achraf Abdelrhafour */
+/* UCID: 30022366 */
+
+let timeLeft = 60; // start with 60 seconds, can be adjusted
+let timerInterval; // timer interval handle
+let players = []; // array to store player information
+let currentPlayerIndex = 0; // index of the current player
+let bridge = []; // array to store bridge information
+
+// Initialize the game with user-defined settings
+function startGame() {
+    // ...
+}
+
+// Update the game's UI based on the current state of the game
+function updateUI() {
+    // ...
+}
+
+// Start a timer that counts down and ends the game if time runs out
+function startTimer() {
+    // ...
+}
+
+// Create a panel representing a portion of the bridge, showing players on it
+function createPanel(lane, position) {
+    // ...
+}
+
+// Move the current player in the chosen direction (left or right)
+function move(direction) {
+    // ...
+}
+
+// Shift to the next player's turn
+function nextPlayer() {
+    // ...
+}
+
+// Skip the current player's turn
+function skipTurn() {
+    // ...
+}
+
+// End the game, usually when the timer runs out
+function endGame() {
+    // ...
+}
